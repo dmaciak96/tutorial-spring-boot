114d8706d3a43f3451cb73a4d04f8db4ef889148
Dawid <maciak.dawid.96@gmail.com>
2023-11-06 12:51:26 +0100

    Add primary annotation to BaseballCoach

--- src/main/java/com/example/springboottutorial/model/BaseballCoach.java
+++ src/main/java/com/example/springboottutorial/model/BaseballCoach.java
@@ -1,7 +1,9 @@
 package com.example.springboottutorial.model;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 public class BaseballCoach implements Coach{
     @Override
