89bf319556e1d09c6f1dbc88d39f9ff12126bd0c
Dawid <maciak.dawid.96@gmail.com>
2023-11-28 15:35:59 +0100

    Implement GET /api/students/{id} endpoint

--- src/main/java/com/example/springboottutorial/student/StudentController.java
+++ src/main/java/com/example/springboottutorial/student/StudentController.java
@@ -1,8 +1,11 @@
 package com.example.springboottutorial.student;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,5 +22,10 @@ public class StudentController {
     @GetMapping("/students")
     public List<StudentEntity> getAllStudents() {
         return studentDAO.findAll();
+    }
+
+    @GetMapping("/students/{id}")
+    public StudentEntity getStudentById(@PathVariable int id) {
+        return studentDAO.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
