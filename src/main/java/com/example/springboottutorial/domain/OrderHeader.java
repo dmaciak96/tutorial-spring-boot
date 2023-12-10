212d15a7f8c759ca2a52aa179d686ae87acbf451
Dawid <maciak.dawid.96@gmail.com>
2023-12-10 17:42:16 +0100

    Implement @Embeddable Address class

--- src/main/java/com/example/springboottutorial/domain/OrderHeader.java
+++ src/main/java/com/example/springboottutorial/domain/OrderHeader.java
@@ -1,13 +1,28 @@
 package com.example.springboottutorial.domain;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 import java.util.Objects;
 
 @Entity
+@AttributeOverrides({
+        @AttributeOverride(name = "shippingAddress.address", column = @Column(name = "shipping_address")),
+        @AttributeOverride(name = "shippingAddress.city", column = @Column(name = "shipping_city")),
+        @AttributeOverride(name = "shippingAddress.state", column = @Column(name = "shipping_state")),
+        @AttributeOverride(name = "shippingAddress.zipCode", column = @Column(name = "shipping_zip_code")),
+        @AttributeOverride(name = "billToAddress.address", column = @Column(name = "bill_to_address")),
+        @AttributeOverride(name = "billToAddress.city", column = @Column(name = "bill_to_city")),
+        @AttributeOverride(name = "billToAddress.state", column = @Column(name = "bill_to_state")),
+        @AttributeOverride(name = "billToAddress.zipCode", column = @Column(name = "bill_to_zip_code"))
+})
 public class OrderHeader extends BaseEntity {
 
     private String customerName;
+    private Address shippingAddress;
+    private Address billToAddress;
 
     public OrderHeader() {
     }
@@ -30,11 +45,11 @@ public class OrderHeader extends BaseEntity {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         OrderHeader that = (OrderHeader) o;
-        return Objects.equals(customerName, that.customerName);
+        return Objects.equals(customerName, that.customerName) && Objects.equals(shippingAddress, that.shippingAddress) && Objects.equals(billToAddress, that.billToAddress);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), customerName);
+        return Objects.hash(super.hashCode(), customerName, shippingAddress, billToAddress);
     }
 }
