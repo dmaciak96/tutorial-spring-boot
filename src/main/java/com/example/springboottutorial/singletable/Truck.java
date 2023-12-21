24144ed48b27d61f338c734e73733aa1dd66a5e6
Dawid <maciak.dawid.96@gmail.com>
2023-12-21 17:03:50 +0100

    Add Single Table

---
+++ src/main/java/com/example/springboottutorial/singletable/Truck.java
@@ -0,0 +1,18 @@
+package com.example.springboottutorial.singletable;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("truck")
+public class Truck extends Vehicle {
+    private Integer payload;
+
+    public Integer getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Integer payload) {
+        this.payload = payload;
+    }
+}
